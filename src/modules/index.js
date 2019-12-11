649ec04611426117d73b616e2a7956d4798a2a4e
afeiship <1290657123@qq.com>
2019-12-11 19:56:17 +0800

    add: box

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,5 +1,6 @@
 var padding = require("./padding");
 var margin = require("./margin");
 var radius = require("./radius");
+var box = require("./box");
 
-module.exports = Object.assign({}, padding, margin, radius);
+module.exports = Object.assign({}, padding, margin, radius, box);
